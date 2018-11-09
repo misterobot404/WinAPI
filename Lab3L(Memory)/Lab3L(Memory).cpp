7a1fb9a302f623ab53026f6af616508d7073faf6
misterobot404 <misterobot404@gmail.com>
2018-11-09 20:16:23 +1000

    h

--- Lab3L(Memory)/Lab3L(Memory).cpp
+++ Lab3L(Memory)/Lab3L(Memory).cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
-#include <iostream>
+#include <string>
 LONG WINAPI WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance,
@@ -7,10 +7,11 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	LPSTR     lpCmdLine,
 	int       nCmdShow)
 {	
-	++hInstance;
-	++hInstance;
-	++hInstance;
 	HWND hwnd;
+
+	byte *ptr = (byte*)hInstance;
+	VirtualProtect(ptr, sizeof(byte), PAGE_READONLY, NULL);
+	
 	MSG msg;
 	WNDCLASS w;
 	memset(&w, 0, sizeof(WNDCLASS));
@@ -22,8 +23,21 @@ int WINAPI WinMain(HINSTANCE hInstance,
 	RegisterClass(&w);
 	hwnd = CreateWindow("My Class", "My title", WS_OVERLAPPEDWINDOW,
 		300, 200, 200, 180, NULL, NULL, hInstance, NULL);
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
+
+	std::string ch;
+	std::string fnl;
+	
+	ch = std::to_string(ptr[0]);
+
+	for (size_t i = 1; i < 5; i++) 
+	{
+		ch += "   ";
+		ch += std::to_string(ptr[i]);
+	}
+		
+
+	MessageBox(hwnd, LPSTR(ch.c_str()),"Первые 5 байтов код. сегмента", 0);
+
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
